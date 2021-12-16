0fee2b7821ca6b5d83873cd88fa6e3967ae6aac6
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-12-17 00:12:19 +0100

    [INIT]: installed tailwind css 3.0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import { Counter } from './features/counter/Counter';
+import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-red-300">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
@@ -18,8 +18,7 @@ function App() {
             className="App-link"
             href="https://reactjs.org/"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             React
           </a>
           <span>, </span>
@@ -27,8 +26,7 @@ function App() {
             className="App-link"
             href="https://redux.js.org/"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             Redux
           </a>
           <span>, </span>
@@ -36,8 +34,7 @@ function App() {
             className="App-link"
             href="https://redux-toolkit.js.org/"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             Redux Toolkit
           </a>
           ,<span> and </span>
@@ -45,8 +42,7 @@ function App() {
             className="App-link"
             href="https://react-redux.js.org/"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             React Redux
           </a>
         </span>
