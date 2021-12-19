eb7f03ad2e39c97a5eadd84f52d78c65ea26481b
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-12-19 01:00:19 +0100

    [ADD]: container centering

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: '1rem',
+    },
   },
   plugins: [],
 };
