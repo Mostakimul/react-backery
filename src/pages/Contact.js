e9a360a288c69dc0b11885b08a90e9259f766a52
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-12-24 23:20:35 +0100

    [ADD]: contact page updated with sections

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import DirectContact from '../components/contact/DirectContact';
+import Form from '../components/contact/Form';
 import Layout from '../layout/Layout';
 
 const Contact = () => {
   return (
     <Layout>
-      <h2>Contact page</h2>
+      <main className="bg-gray-100">
+        <section className="container py-10">
+          <div className="grid sm:grid-cols-8 md:grid-cols-12 gap-5">
+            <div className="sm:col-span-5 md:col-span-7">
+              <Form />
+            </div>
+            <div className="sm:col-span-3 md:col-span-5">
+              <DirectContact />
+            </div>
+          </div>
+        </section>
+      </main>
     </Layout>
   );
 };
