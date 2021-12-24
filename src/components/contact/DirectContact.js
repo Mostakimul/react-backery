982e77ba0f7bbfaf9e722ab21fb80ee2dcb7acd2
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-12-24 23:20:56 +0100

    [ADD]: direct contact section added

---
+++ src/components/contact/DirectContact.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const DirectContact = () => {
+  return (
+    <div>
+      <h2 className="uppercase font-semibold text-yellow-800">...Or, Contact us directly</h2>
+      <div className="text-gray-500">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua enim ad minim veniam.
+        </p>
+        <p>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+          nulla pariatur excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+          deserunt.
+        </p>
+        <div className="mt-5">
+          <p>Phone: 0 800 500 55 123 465</p>
+          <p>Address: Johny Bravo, Street Number, City</p>
+          <p>Email: admin@misti.com</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DirectContact;
