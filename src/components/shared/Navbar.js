2d805feca678bb3d1a273c96108b4811df37cb24
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-12-26 03:35:30 +0100

    [ADD]: cart page and reusable icon added

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { CartIcon } from './CartIcon';
 
 const Navbar = () => {
   // handling navigation
@@ -43,24 +44,7 @@ const Navbar = () => {
 
       {/* mobile humberger icon */}
       <div onClick={handleNav} className="md:hidden flex items-center">
-        <div className="relative mr-5">
-          <svg
-            className="w-6 h-6 text-gray-600"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-            />
-          </svg>
-          <div className="absolute -top-2 -right-4 text-sm rounded-full w-5 h-5 text-center bg-yellow-400 text-gray-50">
-            1
-          </div>
-        </div>
+        <CartIcon isMobile={true} />
         {isNavShowing ? (
           <svg
             className="w-6 h-6 text-gray-600"
@@ -118,24 +102,7 @@ const Navbar = () => {
             {item.name}
           </NavLink>
         ))}
-        <div className="relative">
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-            />
-          </svg>
-          <div className="absolute -top-2 -right-4 text-sm rounded-full w-5 h-5 text-center bg-yellow-400 text-gray-50">
-            1
-          </div>
-        </div>
+        <CartIcon isMobile={false} />
       </nav>
     </div>
   );
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PageHeader from '../components/shared/PageHeader';
 import Layout from '../layout/Layout';
 
 const Cart = () => {
   return (
     <Layout>
-      <h2>Cart Page</h2>
+      <PageHeader title="Cart" />
+      <section className="container">
+        <table class="table-auto w-3/4 mx-auto my-10 border-collapse border border-gray-300">
+          <thead className="text-center bg-gray-300">
+            <tr>
+              <th className="py-5">Product</th>
+              <th className="py-5">Price</th>
+              <th className="py-5">Quantity</th>
+              <th className="py-5">Total</th>
+            </tr>
+          </thead>
+          <tbody className="text-center">
+            <tr>
+              <td className="py-3 border border-collapse border-gray-300">Ciabatta</td>
+              <td className="py-3 border border-collapse border-gray-300">$4.50</td>
+              <td className="py-3 border border-collapse border-gray-300">1</td>
+              <td className="py-3 border border-collapse border-gray-300">$4.50</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="flex justify-end my-10">
+          <Link
+            to={'/checkout'}
+            className="uppercase text-yellow-900 py-1 px-3 border-2 border-yellow-900">
+            Proceed To Checkout
+          </Link>
+        </div>
+      </section>
     </Layout>
   );
 };
