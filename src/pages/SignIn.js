b88a5d71b9d19c821d84ec56452d3d23859b359d
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-12-26 02:04:45 +0100

    [ADD]: login page completed

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import PageHeader from '../components/shared/PageHeader';
 import Layout from '../layout/Layout';
 
 const SignIn = () => {
   return (
     <Layout>
-      <h2>Sign in page!</h2>
+      <PageHeader title="Login" />
+      <section className="bg-gray-100 py-10">
+        <div className="container">
+          <form className="w-2/4 mx-auto space-y-5">
+            <div>
+              <input type="email" placeholder="Email" className="w-full p-2" />
+            </div>
+            <div>
+              <input type="password" placeholder="Password" className="w-full p-2" />
+            </div>
+            <button className="bg-yellow-500 text-white uppercase p-2 font-semibold">Login</button>
+          </form>
+        </div>
+      </section>
     </Layout>
   );
 };
