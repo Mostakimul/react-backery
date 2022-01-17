8f4432336f85ad26fa8e242f0cca5c66ff54a32c
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2022-01-17 22:20:42 +0100

    [FIX]: cart icon position

--- src/components/shared/CartIcon.js
+++ src/components/shared/CartIcon.js
@@ -17,7 +17,7 @@ export const CartIcon = ({ isMobile }) => {
           d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
         />
       </svg>
-      <div className="absolute -top-2 -right-4 text-sm rounded-full w-5 h-5 text-center bg-yellow-400 text-gray-50">
+      <div className="absolute -top-2 -right-2 text-sm rounded-full w-5 h-5 text-center bg-yellow-400 text-gray-50">
         1
       </div>
     </Link>
